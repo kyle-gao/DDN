24d0dd3cff606cd85109e24a6e933373216c6ed6
Yi Lin (Kyle) Gao <67448832+kyle-gao@users.noreply.github.com>
2021-02-26 15:03:41 -0500

    Update loss.py

--- loss.py
+++ loss.py
@@ -16,7 +16,7 @@ def DS_loss_fn(y_true, y_pred, loss_function = entropy, weights=[3,1,1,1,1]):
   for i in range(len(weights)):
 
     #loss = loss+ tf.math.reduce_mean(weights[i]*loss_function(y_true,y_pred[i]) + weights[i]*dice_loss (y_true, y_pred[i]))
-    loss = loss+ tf.math.reduce_mean(weights[i]*dice_loss (y_true, y_pred[i]))
-    #loss = loss+ weights[i]*loss_function(y_true,y_pred[i])
+    #loss = loss+ tf.math.reduce_mean(weights[i]*dice_loss (y_true, y_pred[i]))
+    loss = loss+ weights[i]*loss_function(y_true,y_pred[i])
     y_true = tf.keras.layers.MaxPool2D()(y_true) 
   return loss
